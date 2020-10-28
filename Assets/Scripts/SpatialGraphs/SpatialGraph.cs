295dd43c1898df12461508ee4836e4fde6fee4eb
Sorrer <sorrer@gmail.com>
2020-10-27 21:19:13 -0400

    Added spatial graph interface

---
+++ Assets/Scripts/SpatialGraphs/SpatialGraph.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct SpatialObject {
+    
+    public Vector3 origin; //Center of all points
+    public Vector3 bounding_box; //Size of bounding box
+
+    public Prism prism; //Contains the data for the object
+
+
+}
+
+
+public interface SpatialGraph {
+    void Insert(SpatialObject spatialObject);
+    void Remove(SpatialObject spatialObject);
+    SpatialObject[] GetNeighbors(SpatialObject spatialObject);
+}
