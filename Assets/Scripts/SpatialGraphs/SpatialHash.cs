a4251230ff69193a0ecd5377375bdab411be9219
Jared Tulayan <jared@jaredtulayan.xyz>
2020-10-28 15:51:52 -0400

    Begin work with Spatial Hash

---
+++ Assets/Scripts/SpatialGraphs/SpatialHash.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatialHash : SpatialGraph {
+    Dictionary<int, ArrayList<Prism>> prisms;
+    int width = 0;
+    int length = 0;
+    int height = 0;
+    int cellSize = 0;
+
+    public SpatialHash(int w, int l, int h, int c) {
+        width = w;
+        length = l;
+        height = h;
+        cellSize = c;
+    }
+
+    public void Insert(Prism p) {
+        
+    }
+
+    public void Remove(Prism p) {
+
+    }
+
+    private int hash(Vector2 v) {
+        
+    }
+
+    public Prism[] GetNeighbors(Prism p) {
+        Prism[] ret = {};
+
+        return ret;
+    }
+}
